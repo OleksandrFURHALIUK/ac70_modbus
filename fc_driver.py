e9f663fd7a6092faca812792080de121230fca45
OleksandrFURHALIUK <bashnya.skal@gmail.com>
2024-01-11 00:32:51 +0200

    start stop driver checked

--- fc_driver.py
+++ fc_driver.py
@@ -7,14 +7,33 @@ from pymodbus.client import ModbusTcpClient
 from pymodbus.transaction import ModbusRtuFramer
 
 
-def start_fc(ip_addr: str):
+def debug_fc(ip_addr: str):
     logger.info(f'start fc cmd')
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
-        read = client.write_register(address=8193, value=18,  slave=1)
-        # read = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
-        print(read.registers)
+        if success:
+            # read = client.write_register(address=0x3003, value=2,  slave=1)
+            # read = client.write_register(address=0x3001, value=2, slave=1)
+            read = client.read_holding_registers(address=0x3003, count=1, unit=1, slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
+
+
+def start_fc(ip_addr: str):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            # read = client.write_register(address=8193, value=18,  slave=1)
+            resp = client.write_register(address=0x3001, value=2, slave=1)
+            # read = client.read_holding_registers(address=0x3001, count=1, unit=1, slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
     except Exception as e:
         logger.error(f'{str(e)}')
 
@@ -24,84 +43,144 @@ def stop_fc(ip_addr: str):
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
-        read = client.write_register(address=8193, value=0, slave=1)
-        # read = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
-        print(read.registers)
+        if success:
+            resp = client.write_register(address=0x3001, value=0x005, slave=1)
+            # resp = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
     except Exception as e:
         logger.error(f'{str(e)}')
 
 
-def set_frequency(ip_addr: str, frequency: int):
+def set_frequency(ip_addr: str, frequency):
     logger.info('setting frequency')
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
-        read = client.write_register(address=8192, value=int(frequency*100),  slave=1)
-        # read = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
-        print(read.registers)
+        if success:
+            resp = client.write_register(address=0x3000, value=int(float(frequency) * 100), slave=1)
+            # read = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
     except Exception as e:
         logger.error(f'{str(e)}')
 
 
-def set_rpm(ip_addr: str, rpm: int):
-    logger.info('setting rpm')
+def set_speed(ip_addr: str, speed):
+    speed = int(speed)
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
-        read = client.write_register(address=8192, value=int(rpm), slave=1)
-        # read = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
-        print(read.registers)
+        if success:
+            # read factory motor speed
+            resp = client.write_register(address=0x336, value=int(speed), slave=1)
+            factory_motor_speed = int(resp.registers[0])
+            # read factory motor frequency
+            resp = client.write_register(address=0x335, value=int(speed), slave=1)
+            factory_motor_frequency = int(resp.registers[0])/100
+            # calculate target frequency
+            target_frequency = (factory_motor_frequency/factory_motor_speed)*speed
+            # write target frequency to bus
+            resp = client.write_register(address=0x3000, value=int(target_frequency * 100), slave=1)
+
+            # resp = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
     except Exception as e:
         logger.error(f'{str(e)}')
 
-def motor_init(ip_addr: str, voltage, current, power, frequency, rpm):
-    logger.info('motor init')
+
+def motor_init(ip_addr: str, voltage, current, power, frequency, speed):
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
-        read = client.write_register(address=0x337, value=int(voltage), slave=1)
-        read = client.write_register(address=0x338, value=int(float(current)*100), slave=1)
-        read = client.write_register(address=0x334, value=int(float(power)*100), slave=1)
-        read = client.write_register(address=0x335, value=int(float(frequency)*100), slave=1)
-        read = client.write_register(address=0x336, value=int(rpm), slave=1)
-        print('ok')
+        if success:
+            resp = client.write_register(address=0x337, value=int(voltage), slave=1)
+            resp = client.write_register(address=0x338, value=int(float(current) * 100), slave=1)
+            resp = client.write_register(address=0x334, value=int(float(power) * 100), slave=1)
+            resp = client.write_register(address=0x335, value=int(float(frequency) * 100), slave=1)
+            resp = client.write_register(address=0x336, value=int(speed), slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
     except Exception as e:
         logger.error(f'{str(e)}')
+
 
 def get_motor_data(ip_addr: str):
-    logger.info('get motor data')
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
-        voltage= client.read_holding_registers(address=0x337, count=1, unit=1, slave=1)
-        current = client.read_holding_registers(address=0x338, count=1, unit=1, slave=1)
-        power = client.read_holding_registers(address=0x334, count=1, unit=1, slave=1)
-        frequency = client.read_holding_registers(address=0x335, count=1, unit=1, slave=1)
-        rpm = client.read_holding_registers(address=0x336, count=1, unit=1, slave=1)
-        print(f'voltage: {voltage.registers[0]},\ncurrent: {current.registers[0]/100},\npower: {power.registers[0]/100},\n'
-              f'frequency: {frequency.registers[0]/100},\nrpm: {rpm.registers[0]}')
+        if success:
+            voltage = client.read_holding_registers(address=0x337, count=1, unit=1, slave=1)
+            current = client.read_holding_registers(address=0x338, count=1, unit=1, slave=1)
+            power = client.read_holding_registers(address=0x334, count=1, unit=1, slave=1)
+            frequency = client.read_holding_registers(address=0x335, count=1, unit=1, slave=1)
+            speed = client.read_holding_registers(address=0x336, count=1, unit=1, slave=1)
+            print(
+                f'voltage: {voltage.registers[0]} V\ncurrent: {current.registers[0] / 100} A\npower: {power.registers[0] / 100} kW\n'
+                f'frequency: {frequency.registers[0] / 100} Hz\nspeed: {speed.registers[0]} rpm')
+
+            # todo add set_speed and get current speed
+        else:
+            print('connection to FC failed')
     except Exception as e:
         logger.error(f'{str(e)}')
 
 
-def set_start_duration(ip_addr:str, duration):
-    logger.info('set start duration')
-    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
-    success = client.connect()
-    try:
-        read = client.write_register(address=8192, value=int(duration), slave=1)
-        print('ok')
-    except Exception as e:
-        logger.error(f'{str(e)}')
-
-
-def set_stop_duration(ip_addr:str, duration):
+def set_start_duration(ip_addr: str, duration):
     logger.info('set start duration')
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
     try:
         if success:
-            read = client.write_register(address=8192, value=int(duration), slave=1)
+            resp = client.write_register(address=0x10d, value=int(float(duration) * 100), slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
+
+
+def get_start_duration(ip_addr: str):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            resp = client.read_holding_registers(address=0x10d, count=1, unit=1, slave=1)
+            stop_duration = resp.registers[0]
+            print(f'E-13 Deceleration time 1: {stop_duration / 100}')
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
+
+
+def set_stop_duration(ip_addr: str, duration: str):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            resp = client.write_register(address=0x10e, value=int(float(duration) * 100), slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
+
+
+def get_stop_duration(ip_addr: str):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            resp = client.read_holding_registers(address=0x10e, count=1, unit=1, slave=1)
+            stop_duration = resp.registers[0]
+            print(f'E-14 Deceleration time 1: {stop_duration / 100}')
             print('ok')
         else:
             print('connection to FC failed')
@@ -110,11 +189,80 @@ def set_stop_duration(ip_addr:str, duration):
 
 
 def read_fc_state(ip_addr: str):
-    logger.info(f'read fc info')
     client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
     success = client.connect()
 
-    # read = client.read_holding_registers(address=3076, count=1, unit=1, slave=1)
-    read = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
-    print(read.registers)
+    if success:
+        resp = client.read_holding_registers(address=0x3002, count=1, unit=1, slave=1)
+        value = resp.registers[0]
 
+        print(f'0b{value:08b}')
+    else:
+        print('connection to FC failed')
+
+
+def reset_to_default(ip_addr: str):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            # write parameter E64
+            resp = client.write_register(address=0x140, value=1, slave=1)
+
+            # write parameter E09
+            resp = client.write_register(address=0x109, value=600 * 100, slave=1)
+
+            # write parameter E10
+            resp = client.write_register(address=0x10a, value=600 * 100, slave=1)
+
+            # write parameter E11
+            resp = client.write_register(address=0x10b, value=0, slave=1)
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
+
+
+def switch_control_side_to_bus(ip_addr):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            # write parameter E01
+            read = client.write_register(address=0x101, value=2, slave=1)
+            print('setting E01 to 2')
+
+            # write parameter E02
+            resp = client.write_register(address=0x102, value=6, slave=1)
+            print('setting E02 to 6')
+
+            # write parameter E05
+            resp = client.read_holding_registers(address=0x105, value=1, slave=1)
+            print('setting E05 to 1')
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
+
+
+def switch_control_side_to_lcp(ip_addr):
+    client = ModbusTcpClient(ip_addr, port=9761, framer=ModbusRtuFramer)
+    success = client.connect()
+    try:
+        if success:
+            # write parameter E01
+            read = client.write_register(address=0x101, value=0, slave=1)
+            print('setting E01 to 0')
+            # write parameter E02
+            resp = client.write_register(address=0x102, value=1, slave=1)
+            print('setting E02 to 1')
+            # write parameter E05
+            resp = client.read_holding_registers(address=0x105, value=0, slave=1)
+            print('setting E05 to 0')
+            print('ok')
+        else:
+            print('connection to FC failed')
+    except Exception as e:
+        logger.error(f'{str(e)}')
--- main.py
+++ main.py
@@ -3,18 +3,37 @@ import sys
 
 from loguru import logger
 
-from fc_driver import start_fc, stop_fc, read_fc_state, set_frequency, set_rpm, motor_init, get_motor_data
+from fc_driver import start_fc, stop_fc, read_fc_state, set_frequency, set_speed, motor_init, get_motor_data, \
+    set_start_duration, set_stop_duration, get_start_duration, get_stop_duration, reset_to_default, debug_fc, \
+    switch_control_side_to_lcp, switch_control_side_to_bus
+
+choices = ['start', 'stop',
+           'read_state',
+           'set_frequency', 'set_rpm',
+           'motor_init', 'get_motor_data',
+           'set_start_duration', 'get_start_duration',
+           'set_stop_duration', 'get_stop_duration',
+           'switch_control_side_to_lcp', 'switch_control_side_to_bus',
+           'reset_to_default'
+           ]
+
+epilog = ''
 
 
 def main():
-    parser = argparse.ArgumentParser(description='AC70 frequency converter')
-    parser.add_argument("IP", action='extend', nargs=1, type=str,
+    parser = argparse.ArgumentParser(prog='ac70',
+                                     description='AC70 frequency converter tool',
+                                     epilog='Basic cmd look like: ac70 192.168.3.198 read_state',
+                                     usage='ac70 IP CMD [-CMD_ARGS]'
+                                     )
+    parser.add_argument("IP", action='extend', nargs=1, type=str, metavar='IP',
+                        help='IP address ETH->RS485 converter. For example 192.168.3.198')
+    parser.add_argument('CMD', action='extend', nargs=1, type=str, metavar='CMD', choices=choices,
+                        help=f"Command from available list: {choices}")
+    parser.add_argument('CMD_ARGS', action='extend', nargs='*', type=str, metavar='CMD_ARGS',
+                        help='Command specific arguments')
 
-                        help='IP address to converter. For example 192.168.3.1')
-    parser.add_argument('CMD', action='extend', nargs='+', type=str,
-                        help='CMD')
     args = parser.parse_args()
-    print(args)
 
     if 'start' in args.CMD:
         ip_addr = args.IP[0]
@@ -30,27 +49,69 @@ def main():
 
     elif 'set_frequency' in args.CMD:
         ip_addr = args.IP[0]
-        frequency = args.CMD[0]
+        if not args.CMD_ARGS:
+            print('missing frequency value')
+            return
+        frequency = args.CMD_ARGS[0]
         set_frequency(ip_addr=ip_addr, frequency=frequency)
 
     elif 'set_rpm' in args.CMD:
         ip_addr = args.IP[0]
-        rpm = args.CMD[0]
-        set_rpm(ip_addr=ip_addr, rpm=rpm)
+        if not args.CMD_ARGS:
+            print('missing rpm value')
+            return
+        speed = args.CMD_ARGS[0]
+        set_speed(ip_addr=ip_addr, speed=speed)
 
     elif 'motor_init' in args.CMD:
-        ip_addr = args.IP[0]
-        voltage = args.CMD[1]
-
-        current = args.CMD[2]
-        power = args.CMD[3]
-        frequency = args.CMD[4]
-        rpm = args.CMD[5]
-        motor_init(ip_addr=ip_addr, voltage=voltage, current=current, power=power, frequency=frequency, rpm=rpm)
+        if not args.CMD_ARGS:
+            print('missing data for motor init')
+            return
+        try:
+            ip_addr = args.IP[0]
+            voltage = args.CMD_ARGS[0]
+            current = args.CMD_ARGS[1]
+            power = args.CMD_ARGS[2]
+            frequency = args.CMD_ARGS[3]
+            speed = args.CMD_ARGS[4]
+        except Exception as e:
+            print('not enough arguments')
+            return
+        motor_init(ip_addr=ip_addr, voltage=voltage, current=current, power=power, frequency=frequency, speed=speed)
     elif 'get_motor_data' in args.CMD:
         ip_addr = args.IP[0]
         get_motor_data(ip_addr=ip_addr)
+    elif 'set_start_duration' in args.CMD:
+        ip_addr = args.IP[0]
+        if not args.CMD_ARGS:
+            print('not enough arguments')
+            return
+        set_start_duration(ip_addr=ip_addr, duration=args.CMD_ARGS[0])
+    elif 'get_start_duration' in args.CMD:
+        ip_addr = args.IP[0]
+        get_start_duration(ip_addr=ip_addr)
+    elif 'set_stop_duration' in args.CMD:
+        ip_addr = args.IP[0]
+        if not args.CMD_ARGS:
+            print('not enough arguments')
+            return
+        set_stop_duration(ip_addr=ip_addr, duration=args.CMD_ARGS[0])
+    elif 'get_stop_duration' in args.CMD:
+        ip_addr = args.IP[0]
+        get_stop_duration(ip_addr=ip_addr)
+    elif 'reset_to_default' in args.CMD:
+        ip_addr = args.IP[0]
+        reset_to_default(ip_addr=ip_addr)
+    elif 'switch_control_side_to_lcp' in args.CMD:
+        ip_addr = args.IP[0]
+        switch_control_side_to_lcp(ip_addr=ip_addr)
+    elif 'switch_control_side_to_bus' in args.CMD:
+        ip_addr = args.IP[0]
+        switch_control_side_to_bus(ip_addr=ip_addr)
 
+    elif 'debug' in args.CMD:
+        ip_addr = args.IP[0]
+        debug_fc(ip_addr=ip_addr)
 
 
 if __name__ == '__main__':
